2a05d50a321ef3b57fce6be32159e8392972dcad
KellyOvO <hemanchingkelly@gmail.com>
2025-10-03 18:23:19 +0800

    MAIN

--- code.js
+++ code.js
@@ -6,5 +6,17 @@ var button = document.querySelector("button");
 button.addEventListener("click", myfunction);
 
 function myfunction() {
-    alert("Let me tell you more about myself!");
+    alert("BOOOOOOMMMMMMMMM!!!");
+}
+
+var mynode = document.createElement("div");
+//change basic attributes
+mynode.id = "work1_intro"
+mynode.innerHTML = "The work is an exhibition";
+mynode.style.color = "blue";
+//add event listener
+mynode.addEventListener("click", welcomeToWork1);
+document.querySelector("#my_work1").appendChild(mynode);
+function welcomeToWork1() {
+    mynode.innerHTML = "Welcome to my first work!";
 }
